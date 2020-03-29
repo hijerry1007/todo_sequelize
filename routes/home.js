b8e27970285d604eb2afb9fde56b899b4c96664d
Jerry Lai <jerrydavid123@hotmail.com>
2020-03-29 20:38:17 +0800

    add todo CRUD

--- routes/home.js
+++ routes/home.js
@@ -1,4 +1,3 @@
-// routes/home.js
 
 const express = require('express')
 const router = express.Router()
@@ -14,7 +13,18 @@ const { authenticated } = require('../config/auth')
 // 設定首頁路由
 // 列出全部 Todo
 router.get('/', authenticated, (req, res) => {
-  res.send('列出全部 Todo')
+  User.findByPk(req.user.id)
+    .then((user) => {
+      if (!user) throw new Error("user not found")
+
+      return Todo.findAll({
+        raw: true,
+        nest: true,
+        where: { UserId: req.user.id }
+      })
+    })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
 
 module.exports = router
